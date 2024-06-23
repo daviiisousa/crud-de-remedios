c77a49b83f53ac0bf76e1270c95553a06dd647c5
daviiisousa <157535081+daviiisousa@users.noreply.github.com>
2024-06-23 11:40:39 -0300

    criei tabela

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-
-function enviar(){
-    let nomeRemedio = document.getElementById('nomeRemedio').value;
-    let quantidade = document.getElementById('quantidade').value;
-    let resposta = document.getElementById('res')
-    // resposta.innerHTML = 
-    // `<table  class="table table-hover mt-3"> 
-    //         <tr>
-    //             <th>Remedio</th>
-    //             <th>Quantidade</th>
-    //         </tr>
-    //         <tr>
-    //             <td>${nomeRemedio}</td>
-    //             <td>${quantidade}</td>
-    //         </tr>
-    // </table>`
-}
+// o cosigo abaixo nao deu certo
+// function enviar(){
+//     let nomeRemedio = document.getElementById('nomeRemedio').value;
+//     let quantidade = document.getElementById('quantidade').value;
+//     let resposta = document.getElementById('res')
+//     resposta.innerHTML = 
+//     `<table  class="table table-hover mt-3"> 
+//             <tr>
+//                 <th>Remedio</th>
+//                 <th>Quantidade</th>
+//             </tr>
+//             <tr>
+//                 <td>${nomeRemedio}</td>
+//                 <td>${quantidade}</td>
+//             </tr>
+//     </table>`
+// }
 
 async function API(){
     const metodos ={
@@ -31,27 +31,28 @@ async function API(){
    const remedios = data;
 
    remedios.map((remedio) =>{
+
     const div = document.createElement('div');
 
-    const p = document.createElement('p');
-    p.innerHTML = `<table class="table table-hover"> 
-    <tr>
-        <th>Remedios<th>
-        <th>quantidade<th>
-    </tr>
-    <tr> 
-        <td> ${remedio.nome} </td>
-         
-    </tr> 
-    
-    </table> `
-
     const quantidade = document.createElement('p')
-    quantidade.innerText = remedio.quantidade;
-   
-    div.appendChild(p);
+    
+    const nome = document.createElement('p');
+    
+    div.appendChild(nome);
     div.appendChild(quantidade);
     resposta.appendChild(div);
+
+    resposta.innerHTML = `<table class="table table-striped table-hover mt-5">
+            <tr>
+                <th>Remedio</th>
+                <th>Quantidade</th>
+            </tr>
+            <tr>
+                <td>${remedio.nome}</td>
+                <td>${remedio.quantidade}</td>
+            </tr>
+        </table>`
+    
    })
 
 }
