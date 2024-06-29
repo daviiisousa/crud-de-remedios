0e005d049097c65727be1a056eb550c8d246dfba
Arthur de Macedo <arthurms1708@gmail.com>
2024-06-29 10:11:19 -0300

    adicionei o cariani

--- js/produto.js
+++ js/produto.js
@@ -29,3 +29,40 @@ function Pedido(){
         `
       });
 }
+
+async function API(){
+    const requestOptions ={
+        method: "GET"
+    };
+
+    let resposta = document.getElementById('res');
+
+   const response = await fetch("http://localhost:3000/Remedios", requestOptions);
+   const data = await response.json();
+
+   console.log(data);
+
+   const remedios = data;
+
+   remedios.map((remedio) =>{
+
+    const div = document.createElement('div');
+
+    const quantidade = document.createElement('p')
+    
+    const nome = document.createElement('p');
+    
+    div.appendChild(nome);
+    div.appendChild(quantidade);
+    resposta.appendChild(div).innerHTML = 
+    `<table class="table table-hover">
+    <tr>
+        <td>${remedio.nome}</td>
+        <td class="w-25">${remedio.quantidade}</td>
+        <td class="w-25"> <button type="button" onclick="pedido()" class="btn btn-outline-primary">Fazer Pedido</button></td> 
+    </tr>
+    </table>
+    `  
+   })
+
+}
